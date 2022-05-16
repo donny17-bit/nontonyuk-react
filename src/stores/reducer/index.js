453e23b9d76ae003b5fff4f7eba89f71bf7724d1
Donny <doniwahyu14@gmail.com>
2022-05-16 22:41:20 +0700

    update order page (without redux)

--- src/stores/reducer/index.js
+++ src/stores/reducer/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import manageMovie from "./manageMovie";
 import schedule from "./schedule";
 import booking from "./booking";
-import user from "./booking";
+import user from "./user";
 
 export default combineReducers({
   manageMovie,
